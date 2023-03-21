8bf8e7483049e6b5ed493f9422f3fb383d3c6ee6
Kelly Chen <kchen@ocadu.ca>
2023-03-21 17:59:36 -0400

    added my second sketch

---
+++ 03.21b/sketch.js
@@ -0,0 +1,33 @@
+function setup(){
+    createCanvas(1000, 1000);
+    background(230);
+
+    strokeWeight(8);
+
+    ellipse(500, 500, 100, 500);
+    fill(0);
+
+    ellipse(500, 500, 500);
+    fill(230);
+
+    ellipse(500, 500, 100, 500);
+    fill(62, 84, 172);
+}
+
+// function draw(){
+//     if (mouseIsPressed) {
+//         // blue circle
+//         ellipse(500, 500, 600); // outer ring
+//         fill(255);
+        
+//         ellipse(500, 500, 400); // inner ring
+//         fill(0, 120, 191, 50, 50);
+//     } else {
+//         // pink circle
+//         ellipse(500, 500, 500); // outer ring
+//         fill(255);
+        
+//         ellipse(500, 500, 300); // inner ring
+//         fill(255, 72, 176, 255);
+//     }
+// }
