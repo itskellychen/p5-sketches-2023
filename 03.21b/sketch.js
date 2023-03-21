dfdd16d9e08df8cd0f48ea2179b9c05f26bc4814
Kelly Chen <kchen@ocadu.ca>
2023-03-21 18:08:39 -0400

    added delusha's letter d practice

--- 03.21b/sketch.js
+++ 03.21b/sketch.js
@@ -13,3 +13,16 @@ function setup(){
     ellipse(500, 500, 100, 500);
     fill(62, 84, 172);
 }
+
+// Trying out Delusha's letter 'D'
+// function setup(){
+//     createCanvas(1000, 1000);
+//     background(255);
+
+//     strokeWeight(15);
+
+//     fill(255);
+//     arc(500, 500, 500, 500, 300, HALF_PI);
+//     fill(0);
+//     rect(400, 250, 100, 500);
+// }
