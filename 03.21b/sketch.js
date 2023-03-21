aea631fef4716ea0eb1f5bd096e7f0cb9a4faa38
Kelly Chen <kchen@ocadu.ca>
2023-03-21 18:00:18 -0400

    updated my second sketch

--- 03.21b/sketch.js
+++ 03.21b/sketch.js
@@ -13,21 +13,3 @@ function setup(){
     ellipse(500, 500, 100, 500);
     fill(62, 84, 172);
 }
-
-// function draw(){
-//     if (mouseIsPressed) {
-//         // blue circle
-//         ellipse(500, 500, 600); // outer ring
-//         fill(255);
-        
-//         ellipse(500, 500, 400); // inner ring
-//         fill(0, 120, 191, 50, 50);
-//     } else {
-//         // pink circle
-//         ellipse(500, 500, 500); // outer ring
-//         fill(255);
-        
-//         ellipse(500, 500, 300); // inner ring
-//         fill(255, 72, 176, 255);
-//     }
-// }
