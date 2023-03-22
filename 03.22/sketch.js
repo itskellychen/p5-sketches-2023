4b6e2053599d96289a6508a8a778399297bb1807
Kelly Chen <kchen@ocadu.ca>
2023-03-22 17:33:43 -0400

    added my third sketch

---
+++ 03.22/sketch.js
@@ -0,0 +1,34 @@
+function setup(){
+    createCanvas(1000, 1000);
+    background(36, 55, 99);
+}
+
+function draw(){
+
+    ellipse(500, 500, 700, 700); // inner ring
+    fill(36, 55, 99);
+    noStroke();
+
+    ellipse(500, 500, 500, 650); // outer ring
+    fill(0, 129, 180);
+    noStroke();
+
+    ellipse(500, 500, 100, 100); // smaller 'o' inside ring
+
+    // if (mouseIsPressed){
+    //     ellipse(mouseX, mouseY, 50, 50);
+    // } else {
+    //     ellipse(500, 500, 700, 700); // inner ring
+    //     fill(36, 55, 99);
+    //     noStroke();
+    
+    //     ellipse(500, 500, 500, 650); // outer ring
+    //     fill(0, 129, 180);
+    //     noStroke();
+        
+    //     ellipse(500, 500, 100, 100); // smaller 'o' inside ring
+    // }
+}
+
+// For next time:
+// https://p5js.org/reference/#/p5/vertex
